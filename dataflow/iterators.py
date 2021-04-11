422bbc50fb8907e0468011082a807f4c57e93b8c
Ioannis Gkinis <igkinis@omilia.com>
2021-04-11 12:21:08 +0300

    add functionality for saving MSH xref to a new table

--- dataflow/iterators.py
+++ dataflow/iterators.py
@@ -2,14 +2,12 @@ from typing import Iterator, Dict, Any
 
 
 def iter_terms_from_api(response) -> Iterator[Dict[str, Any]]:
-
     data = response.json()['_embedded']['terms']
 
     yield from data
 
 
 def iter_synonyms_from_response(response) -> Iterator[Dict[str, Any]]:
-
     metadata = []
     for index, term in enumerate(response.json()['_embedded']['terms']):
         try:
@@ -22,7 +20,6 @@ def iter_synonyms_from_response(response) -> Iterator[Dict[str, Any]]:
 
 
 def iter_parents_from_response(response, session) -> Iterator[Dict[str, Any]]:
-
     metadata = []
     for index, term in enumerate(response.json()['_embedded']['terms']):
         temp_response = session.get(term['_links']['parents']['href']).json()['_embedded']['terms'][0]
@@ -31,3 +28,13 @@ def iter_parents_from_response(response, session) -> Iterator[Dict[str, Any]]:
 
     yield from metadata
 
+
+def iter_xref_from_response(response) -> Iterator[Dict[str, Any]]:
+    metadata = []
+    for index, term in enumerate(response.json()['_embedded']['terms']):
+        for xref in term['obo_xref']:
+            if xref['database'] in {'MESH', 'MSH'}:
+                xref.update({'terms_id': term['short_form']})
+                metadata.append(xref)
+
+    yield from metadata
